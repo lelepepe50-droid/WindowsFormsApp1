72a11fad83d501a8bdc93f3100d8dfdc14aeb5b9
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-05-08 22:19:45 -0300

    Update12

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -32,21 +32,30 @@
             this.btnEditar = new System.Windows.Forms.Button();
             this.btnConsultar = new System.Windows.Forms.Button();
             this.btnLimpar = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvCarro = new System.Windows.Forms.DataGridView();
             this.nome = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txtnome = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.cbtelefone = new System.Windows.Forms.Label();
+            this.placa = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnConsultarLista = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dtpano = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarro)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(1, 298);
+            this.btnSalvar.Location = new System.Drawing.Point(1, 243);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(156, 67);
             this.btnSalvar.TabIndex = 0;
@@ -56,7 +65,7 @@
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(1, 371);
+            this.btnEditar.Location = new System.Drawing.Point(1, 316);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(156, 67);
             this.btnEditar.TabIndex = 1;
@@ -65,16 +74,17 @@
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(163, 298);
+            this.btnConsultar.Location = new System.Drawing.Point(163, 243);
             this.btnConsultar.Name = "btnConsultar";
             this.btnConsultar.Size = new System.Drawing.Size(156, 67);
             this.btnConsultar.TabIndex = 2;
             this.btnConsultar.Text = "btnCONSULTAR";
             this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
             // 
             // btnLimpar
             // 
-            this.btnLimpar.Location = new System.Drawing.Point(163, 371);
+            this.btnLimpar.Location = new System.Drawing.Point(163, 316);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(156, 67);
             this.btnLimpar.TabIndex = 3;
@@ -82,18 +92,18 @@
             this.btnLimpar.UseVisualStyleBackColor = true;
             this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
-            // dataGridView1
+            // dgvCarro
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(351, 298);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(437, 140);
-            this.dataGridView1.TabIndex = 4;
+            this.dgvCarro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarro.Location = new System.Drawing.Point(351, 243);
+            this.dgvCarro.Name = "dgvCarro";
+            this.dgvCarro.Size = new System.Drawing.Size(437, 195);
+            this.dgvCarro.TabIndex = 4;
             // 
             // nome
             // 
             this.nome.AutoSize = true;
-            this.nome.Location = new System.Drawing.Point(12, 13);
+            this.nome.Location = new System.Drawing.Point(25, 13);
             this.nome.Name = "nome";
             this.nome.Size = new System.Drawing.Size(36, 13);
             this.nome.TabIndex = 5;
@@ -101,39 +111,39 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(66, 32);
+            this.textBox1.Location = new System.Drawing.Point(91, 32);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(189, 20);
             this.textBox1.TabIndex = 6;
             // 
             // txtnome
             // 
-            this.txtnome.Location = new System.Drawing.Point(66, 6);
+            this.txtnome.Location = new System.Drawing.Point(91, 6);
             this.txtnome.Name = "txtnome";
             this.txtnome.Size = new System.Drawing.Size(189, 20);
             this.txtnome.TabIndex = 7;
             // 
-            // label2
+            // cbtelefone
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 39);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Telefone:";
+            this.cbtelefone.AutoSize = true;
+            this.cbtelefone.Location = new System.Drawing.Point(24, 39);
+            this.cbtelefone.Name = "cbtelefone";
+            this.cbtelefone.Size = new System.Drawing.Size(52, 13);
+            this.cbtelefone.TabIndex = 8;
+            this.cbtelefone.Text = "Telefone:";
             // 
-            // label4
+            // placa
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 65);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Placa:";
+            this.placa.AutoSize = true;
+            this.placa.Location = new System.Drawing.Point(28, 65);
+            this.placa.Name = "placa";
+            this.placa.Size = new System.Drawing.Size(37, 13);
+            this.placa.TabIndex = 10;
+            this.placa.Text = "Placa:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(66, 65);
+            this.textBox3.Location = new System.Drawing.Point(91, 58);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(189, 20);
             this.textBox3.TabIndex = 11;
@@ -141,7 +151,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Location = new System.Drawing.Point(24, 91);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(45, 13);
             this.label3.TabIndex = 12;
@@ -149,25 +159,108 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(66, 96);
+            this.textBox4.Location = new System.Drawing.Point(91, 84);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(189, 20);
             this.textBox4.TabIndex = 13;
+            // 
+            // btnConsultarLista
+            // 
+            this.btnConsultarLista.Location = new System.Drawing.Point(27, 392);
+            this.btnConsultarLista.Name = "btnConsultarLista";
+            this.btnConsultarLista.Size = new System.Drawing.Size(270, 37);
+            this.btnConsultarLista.TabIndex = 14;
+            this.btnConsultarLista.Text = "btnConsultarLista";
+            this.btnConsultarLista.UseVisualStyleBackColor = true;
+            this.btnConsultarLista.Click += new System.EventHandler(this.btnConsultarLista_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Marca:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // dtpano
+            // 
+            this.dtpano.AutoSize = true;
+            this.dtpano.Location = new System.Drawing.Point(32, 146);
+            this.dtpano.Name = "dtpano";
+            this.dtpano.Size = new System.Drawing.Size(29, 13);
+            this.dtpano.TabIndex = 17;
+            this.dtpano.Text = "Ano:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Cor:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(91, 165);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(189, 20);
+            this.textBox6.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Quilometragem:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(91, 191);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(189, 20);
+            this.textBox7.TabIndex = 22;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(91, 140);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(189, 20);
+            this.dateTimePicker1.TabIndex = 23;
             // 
             // Telasvelculos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 441);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtpano);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConsultarLista);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.placa);
+            this.Controls.Add(this.cbtelefone);
             this.Controls.Add(this.txtnome);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.nome);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvCarro);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnConsultar);
             this.Controls.Add(this.btnEditar);
@@ -175,7 +268,7 @@
             this.Name = "Telasvelculos";
             this.Text = "Tela pricinpal";
             this.Load += new System.EventHandler(this.Telasvelculos_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarro)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,15 +280,24 @@
         private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.Button btnConsultar;
         private System.Windows.Forms.Button btnLimpar;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvCarro;
         private System.Windows.Forms.Label nome;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox txtnome;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label cbtelefone;
+        private System.Windows.Forms.Label placa;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnConsultarLista;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label dtpano;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,6 +17,9 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        
+        const string DADOS_CONEXAO =
+        "Server=localhost;user=root;password=;database=projeto";
 
         private void Telasvelculos_Load(object sender, EventArgs e)
         {
@@ -31,11 +34,19 @@ namespace WindowsFormsApp1
         private void btnSalvar_Click(object sender, EventArgs e)
         {
             string campoNome = txtnome.Text;
-            int controlelinhasAfetadas = 0;
+            string campoTelefone = cbtelefone.Text;
+            string campoAno = dtpano.Text;
 
-            string dadosConexao =
-             "Server=localhost;user=root;password=;database=projeto";
-            using (MySqlConnection conn = new MySqlConnection(dadosConexao) )
+            DateTime dateConvertida = DateTime.Parse(campoAno);
+                int controlelinhasAfetadas = 0;
+            MessageBox.Show(
+                $"nome: {campoNome} \n" +
+                $"telofone: {campoTelefone}\n" +
+                $"ano: {campoAno}\n" +
+                $"data Convertida:{dateConvertida}");
+
+            
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
             {
                 conn.Open();
                 string scriptInsert = "Insert INTO tb_projetos (nomeVededor) VALUE (@nomeVededor)";
@@ -56,6 +67,70 @@ namespace WindowsFormsApp1
             else
             {
                 MessageBox.Show("Ops. Algo deu errado!!!");
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
+            {
+                conn.Open();
+                string scriptConsultaIndividual = "SETECT * FROM  tb_cadastro WHERE id = 1";
+                
+                using (MySqlCommand comando = new MySqlCommand(scriptConsultaIndividual, conn))
+                {
+                   var dadosResultado = comando.ExecuteReader();
+
+                   while (dadosResultado.Read())
+                    {
+                       // lbidResultado.text = dadosResultado["id"].ToString();
+                        //lbNomeResultado.text = dadosResultado["nomeVendendor"].ToString();
+                        //lbResultado.text = dadosResultado["telefone"].ToString();
+                        //lbDataResultado.text = dadosResultado["ano"].ToString();
+                    }
+                   conn.Close();
+                }
+            }
+        }
+
+        private void btnConsultarLista_Click(object sender, EventArgs e)
+        {
+           
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
+            {
+                conn.Open();
+
+                string campoServico = txtnome.Text;
+                string scriptConsultaLista = "";
+                if (!string.IsNullOrEmpty(campoServico))
+                {
+                    scriptConsultaLista = $"SELECT * FROM tb_cadastro WHERE nomeVendendor LIKE '%{campoServico}%'";
+                }
+                else
+                {
+                    scriptConsultaLista = "SELECT * FROM tb_cadastro";
+                }
+
+                string scriptCosulta = "SELECT * FROM tb_cadastro";
+
+                using (MySqlCommand comando = new MySqlCommand(scriptCosulta, conn))
+                {
+                    if (!string.IsNullOrEmpty(campoServico))
+                    {
+                       comando.Parameters.AddWithValue("@nomeVendendor", $"%{campoServico}%");
+                    }
+                    MySqlDataAdapter resultadoConsultaMySql = new MySqlDataAdapter(comando);
+
+                    DataTable dt = new DataTable();
+
+                    resultadoConsultaMySql.Fill(dt);
+
+                    dgvCarro.DataSource = dt;
+
+
+
+                }
+               conn.Close();
             }
         }
     }
